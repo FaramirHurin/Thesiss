c37a923bc7ea44a71c0b3537164ad45bc7f4f36b
FaramirHurin <dalunghi@gmail.com>
2021-02-03 17:16:43 +0100

    Yet to be tested: full EWMA threshold computatiion

--- EWMA_QuantTree.py
+++ EWMA_QuantTree.py
@@ -11,22 +11,26 @@ import qtLibrary.libquanttree as qt
 class EWMA_QuantTree:
 
     def __init__(self, initial_pi_values, lamb, statistic, maxN, minN, alpha, stop, does_restart, nu, beta):
-        self.tree = Extended_Quant_Tree(ext.create_bins_combination(len(initial_pi_values), 2 * len(initial_pi_values)))
+        pi_values = ext.create_bins_combination(len(initial_pi_values), 2 * len(initial_pi_values))
+        self.tree = Extended_Quant_Tree(pi_values)
         self.lamb = lamb
         self.nu = nu
         self.statistic = statistic
+        self.record_history = []
         self.alpha = alpha
         self.value = self.alpha[0]
-        self.beta = beta  # sort of alpha for the second level statistic
+        self.beta = beta  # 1/ARL0
         self.status = 0
-        # self.threshold = self.compute_EWMA_threshold(nu)
         self.threshold = 0
+        self.max_len_computed = 0
+        self.EWMA_threshold = 0
+        self.EWMA_thresholds = 0
         self.stop = stop
         self.does_restart = does_restart
         self.buffer = None
-        self.record_history = []
 
     def initialize(self, training_set):
+        self.trainig_set = training_set
         if self.statistic ==  qt.pearson_statistic:
             p = 0
         elif self.statistic == qt.tv_statistic:
@@ -35,21 +39,68 @@ class EWMA_QuantTree:
             raise ('Strange exception: ' + str(self.statistic))
         self.tree.build_histogram(training_set)
         self.threshold = qt.ChangeDetectionTest(self.tree, self.nu, self.statistic).estimate_quanttree_threshold(self.alpha, 10000)
+        val = 0
+        exp = 1
+        """for index in range(exp):
+            val += self.compute_EWMA_threshold()
+        self.EWMA_threshold = val/exp
+        print(self.EWMA_threshold)
+        """
 
-    # MC simulation
-    def compute_EWMA_threshold(self, nu):
-        lenght = 50000
-        statistics = np.zeros(lenght)
-        # Uses its own methods to simulate a run and compute the threshold
-        self.tree.build_histogram(np.random.random(nu))
-        for round in range(lenght):
-            batch = np.random.random(nu)
-            statistics[round] = self.compute_EMWA(batch)
-        values = np.sort(statistics)
-        index = int((1 - self.beta) * lenght)
-        # Restores original values
-        self.tree = qt.QuantTree(self.initial_pi_values)  # Come back to original Pi values
-        return values[index]
+    # MC simulation, constant threshold
+    def compute_EWMA_threshold(self):
+        run_lenght = 1000000
+        tree, threshold = self.prepare_simulated_run()
+        values = self.simulate_EWMA_run(run_lenght, tree, threshold )
+        selection = values[:int(run_lenght*self.beta)]
+        return np.max(selection)
+
+    def alterative_EWMA_thresholds_computation(self):
+        x = None
+        experiments = 100000
+        max_lenght = (1/self.beta) * 20
+        table = self.fill_table(experiments, max_lenght)
+        self.EWMA_thresholds = self.compute_thresholds_on_table(table, [])
+        self.max_len_computed = len(self.EWMA_thresholds)
+        return self.EWMA_thresholds
+
+    def compute_thresholds_on_table(self, table, thresholds):
+        if table.shape[0] < 1/self.beta:
+            return thresholds
+        thresholds = np.zeros(table.shape[1])
+        values = table[:, 0]
+        vals = np.sort(values)
+        threshold = vals[int(len(vals) * (1 - self.beta))]
+        thresholds.append(threshold)
+        table = np.delete(table, np.where(table[0] > threshold), axis=0)
+        table = np.delete(table, 0, axis = 1)
+        thresholds = self.compute_thresholds_on_table(table, thresholds)
+        return thresholds
+
+    def fill_table(self, experiments, max_lenght):
+        table = np.zeros(experiments, max_lenght)
+        tree, threshold = self.prepare_simulated_run()
+        for index in range(experiments):
+            tree, threshold = self.prepare_simulated_run()
+            values = self.simulate_EWMA_run(max_lenght, tree, threshold)
+        return table
+
+    def prepare_simulated_run(self):
+        training_set = np.random.uniform(0, 1, self.tree.ndata)
+        tree = qt.QuantTreeUnivariate(self.tree.pi_values)
+        tree.build_histogram(training_set)
+        threshold = qt.ChangeDetectionTest(tree, self.nu, self.statistic)\
+            .estimate_quanttree_threshold(self.alpha, 4000)
+        return tree, threshold
+
+    def simulate_EWMA_run(self, max_lenght, tree, threshold):
+        values = np.zeros(max_lenght)
+        values[0] = self.alpha[0]
+        for index in range(1, max_lenght):
+            batch = np.random.uniform(0, 1, self.nu)
+            positive = self.statistic(tree, batch) > threshold
+            values[index] = (1 - self.lamb) * values[index - 1] + positive * self.lamb
+        return values
 
     def classic_batch_analysis(self, batch):
         stat = self.statistic(self.tree, batch)
@@ -64,7 +115,6 @@ class EWMA_QuantTree:
         self.value = (1 - self.lamb) * self.value + positive * self.lamb
         return self.value
 
-    def find_change(self, EMWA_value, batch):
+    def find_change(self):
         # If there is a change we return True
-        return EMWA_value > self.threshold
-
+        return self.value > self.EWMA_threshold
