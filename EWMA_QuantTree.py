ff3b26db62430c74c530b0302a0461bdf9794f9a
FaramirHurin <dalunghi@gmail.com>
2021-02-09 20:03:34 +0100

    New EWMA threshold computation

--- EWMA_QuantTree.py
+++ EWMA_QuantTree.py
@@ -10,42 +10,39 @@ import qtLibrary.libquanttree as qt
 
 class EWMA_QuantTree:
 
-    def __init__(self, initial_pi_values, lamb, statistic, maxN, minN, alpha, stop, does_restart, nu, beta):
-        pi_values = ext.create_bins_combination(len(initial_pi_values), len(initial_pi_values))
+    def __init__(self, initial_pi_values, lamb, statistic, alpha, stop, nu, desired_ARL0):
+        pi_values = ext.create_bins_combination(len(initial_pi_values), 3 * len(initial_pi_values))
         self.pi_values = pi_values
         self.tree = Extended_Quant_Tree(pi_values)
         self.lamb = lamb
         self.nu = nu
         self.statistic = statistic
         self.record_history = []
-        self.alpha = [0.4]
+        self.alpha = alpha
         self.value = self.alpha[0]
-        self.beta = 0.01  # 1/ARL0
+        self.beta = 1/desired_ARL0  # 1/ARL0
+        self.desired_ARL0 = desired_ARL0
         self.threshold = 0
         self.max_len_computed = 0
-        self.EWMA_threshold = 0
-        self.EWMA_thresholds = 0
+        #self.EWMA_threshold = 0
+        self.EWMA_thresholds = []
         self.stop = stop
+        self.status = 0
+        self.training_set = None
 
     def initialize(self, training_set):
-        self.trainig_set = training_set
+        self.training_set = training_set
         if self.statistic ==  qt.pearson_statistic:
-            p = 0
+            debug = 0
         elif self.statistic == qt.tv_statistic:
-            p = 1
+            debug = 1
         else:
             raise ('Strange exception: ' + str(self.statistic))
         self.tree.build_histogram(training_set)
         self.threshold = qt.ChangeDetectionTest(self.tree, self.nu, self.statistic).\
             estimate_quanttree_threshold(self.alpha, 10000)
-        """
-        val = 0
-        exp = 1
-        for index in range(exp):
-            val += self.compute_EWMA_threshold()
-        self.EWMA_threshold = val/exp
-        print(self.EWMA_threshold)
-        """
+        self.EWMA_thresholds = self.alterative_EWMA_thresholds_computation()
+    """
     # MC simulation, constant threshold
     def compute_EWMA_threshold(self):
         run_lenght = 1000000
@@ -53,32 +50,38 @@ class EWMA_QuantTree:
         values = self.simulate_EWMA_run(run_lenght, tree, ewma_0)
         selection = values[:int(run_lenght*self.beta)]
         return np.max(selection)
+    """
 
     def alterative_EWMA_thresholds_computation(self):
         x = None
-        experiments = 2000
-        max_lenght = 20
+        max_lenght = 3 * self.desired_ARL0
+        experiments = max_lenght * 30
         table = self.fill_table(experiments, max_lenght)
-        means = np.zeros(table.shape[1])
+        """means = np.zeros(table.shape[1])
         for index in range(len(means)):
             means[index] = np.mean(table[:,index])
         print('means are' + str(means))
+        """
         self.EWMA_thresholds = self.compute_thresholds_on_table(table, None)
         self.max_len_computed = len(self.EWMA_thresholds)
         return self.EWMA_thresholds
 
+    #Recursive calls: appends a threshold to the ones compited so far.
+    #It modifies the table and passes to a new istance of itself
     def compute_thresholds_on_table(self, table, thresholds):
         if table.shape[0] < 1/self.beta or table.shape[1] < 1:
+            # End Cycle
             return thresholds
+        #First call
         if thresholds is None:
             thresholds = []
+
         values = table[:, 0]
         vals = np.sort(values)
         threshold = vals[int(len(vals) * (1 - self.beta))]
-        if threshold < np.mean(vals) - 0.0001: #Costante necessaria per gli arrotondamenti
-            print('Vals are: ' + str(vals))
-            print('Threshold is' + str(threshold))
-            raise('Bad threshold comutation')
+
+        assert threshold > np.mean(vals) - 0.0001 #Costante necessaria per gli arrotondamenti
+
         thresholds.append(threshold)
         to_eliminate = []
         for index in range(table.shape[0]):
@@ -86,18 +89,19 @@ class EWMA_QuantTree:
                 to_eliminate.append(index)
         table = np.delete(table, to_eliminate, axis=0)
         table = np.delete(table, 0, axis = 1)
+        #Recursive call
         thresholds = self.compute_thresholds_on_table(table, thresholds)
         return thresholds
 
     def fill_table(self, experiments, max_lenght):
-        indipendence_percentage = 0.0001
+        indipendence_percentage = 0.00001
         table = np.zeros([experiments, max_lenght - 1])
         for index in range(experiments):
-            if index % 10 == 0:
+            if index % 100 == 0:
                 print(index)
-                if index * indipendence_percentage %1 == 0:
-                    tree, ewma_0 = self.prepare_simulated_run()
-                    print('simulated')
+            if index * indipendence_percentage %1 == 0:
+                tree, ewma_0 = self.prepare_simulated_run()
+                print('simulated')
             values = self.simulate_EWMA_run(max_lenght, tree, ewma_0)
             table[index] = values[1:]
         table[:, 0] = ewma_0
@@ -107,12 +111,13 @@ class EWMA_QuantTree:
         training_set = np.random.uniform(0, 1, self.tree.ndata)
         tree = qt.QuantTreeUnivariate(self.tree.pi_values)
         tree.build_histogram(training_set)
-        poses = 0
-        for index in range(4000):
+        signaled_batches = 0
+        simulations_number = 4000
+        for index in range(simulations_number):
             batch = np.random.uniform(0, 1, self.nu)
             positive = self.classic_batch_analysis(batch, tree)
-            poses += positive
-        ewma_0 = poses/index
+            signaled_batches += positive
+        ewma_0 = signaled_batches/simulations_number
         return tree, ewma_0
 
     def simulate_EWMA_run(self, max_lenght, tree, ewma_0):
@@ -131,13 +136,18 @@ class EWMA_QuantTree:
         positive =  stat > self.threshold
         self.record_history.append(positive)
         return positive
+
     # Compute current value of EMWA based on the value at previous round.
     # Important: it updates the attributes of the object
     def compute_EMWA(self, batch):
-        positive = self.classic_batch_analysis(batch)
+        positive = self.classic_batch_analysis(batch, None)
         self.value = (1 - self.lamb) * self.value + positive * self.lamb
+        self.status += 1
         return self.value
 
     def find_change(self):
         # If there is a change we return True
-        return self.value > self.EWMA_threshold
+        index = min(len(self.EWMA_thresholds) - 1, self.status)
+        change = self.value > self.EWMA_thresholds[index]
+        return change
+
--- tests/dynamicTest.py
+++ tests/dynamicTest.py
@@ -4,28 +4,25 @@ import qtLibrary.libquanttree as qt
 import numpy as np
 import matplotlib.pyplot as plt
 
-bins_number = 16
+bins_number = 8
 initial_pi_values = np.ones(bins_number)/bins_number
 data_number = 2000
 alpha = [0.5]
-beta = 0.1
+desired_ARL0 = 40
 lambd = 0.01
 data_Dimension = 5
 nu = 32
 B = 4000
 X = [3]
-max_N = 3000
-min_N = nu
 SKL = 1
-
 
 #Dynamic QuantTree tests
 
-def run_normally(run_lenght, beta, statistic):
+def run_normally(run_lenght, desired_ARL0, statistic):
     data_handler = aux.Data_set_Handler(data_Dimension)
     data = data_handler.return_equal_batch(500)
     statistics = np.zeros(run_lenght)
-    x = EWMA_QuantTree.EWMA_QuantTree(initial_pi_values, lambd, statistic, max_N, 100, alpha, False, False, nu, beta)
+    x = EWMA_QuantTree.EWMA_QuantTree(initial_pi_values, lambd, statistic, alpha, False, nu, desired_ARL0)
     x.initialize(data)
     for round in range(run_lenght):
         batch = data_handler.return_equal_batch(nu)
@@ -33,11 +30,11 @@ def run_normally(run_lenght, beta, statistic):
     return np.mean(statistics)
 
 #Plots the EWMA value of one run.
-def test_EWMA_once(run_lenght, beta, statistic):
+def test_EWMA_once(run_lenght, desired_ARL0, statistic):
     data_handler = aux.Data_set_Handler(data_Dimension)
     data = data_handler.return_equal_batch(500)
     statistics = np.zeros(run_lenght)
-    x = EWMA_QuantTree.EWMA_QuantTree(initial_pi_values, lambd, statistic, max_N, 100, alpha, False, False, nu, beta)
+    x = EWMA_QuantTree.EWMA_QuantTree(initial_pi_values, lambd, statistic, alpha, False,  nu, desired_ARL0)
     x.initialize(data)
     for round in range(run_lenght):
         batch = data_handler.return_equal_batch(nu)
@@ -48,11 +45,11 @@ def test_EWMA_once(run_lenght, beta, statistic):
 #Taking random points of independent runs the EMWA is higher than the threshold with probability alpha
 
 #Averages over multiple runs. We expect the EWMA to be around alpha for every time T
-def test_EWMA(experiments_number, run_lenght, beta, statistic):
+def test_EWMA(experiments_number, run_lenght, desired_ARL0, statistic):
     statistics = np.zeros(run_lenght)
     record_history = np.zeros(run_lenght)
     for exp in range(experiments_number):
-        to_add, record_history_to_add =  test_EWMA_once(run_lenght, beta, statistic)
+        to_add, record_history_to_add =  test_EWMA_once(run_lenght, desired_ARL0, statistic)
         record_history = record_history + np.array(record_history_to_add)
         statistics = statistics + to_add
         print(record_history_to_add)
@@ -68,10 +65,10 @@ def test_EWMA(experiments_number, run_lenght, beta, statistic):
 
 def test_ARLO_once(run_lenght, statistic):
     data_handler = aux.Data_set_Handler(data_Dimension)
-    data = data_handler.return_equal_batch(500)
+    data = data_handler.return_equal_batch(4000)
     statistics = np.zeros(run_lenght)
     x = EWMA_QuantTree.EWMA_QuantTree\
-        (initial_pi_values, lambd, statistic, max_N, 100, alpha, False, False, nu, beta)
+        (initial_pi_values, lambd, statistic, alpha, False, nu, desired_ARL0)
     x.initialize(data)
     for round in range(run_lenght):
         batch = data_handler.return_equal_batch(nu)
@@ -80,15 +77,29 @@ def test_ARLO_once(run_lenght, statistic):
             break
     return round
 
-model = EWMA_QuantTree.EWMA_QuantTree(initial_pi_values, lambd, qt.tv_statistic, max_N, 100, alpha, False, False, nu, beta)
-data_handler = aux.Data_set_Handler(data_Dimension)
-data = data_handler.return_equal_batch(2000)
-model.initialize(data)
-stat = np.array(model.alterative_EWMA_thresholds_computation())
-for index in range(1, 10):
-    stat += np.array(model.alterative_EWMA_thresholds_computation())
-stat = stat/index
-plt.plot(stat)
-plt.title('thresholds')
+def plot_EWMA_thresholds(initial_data_set, experiments):
+    model = EWMA_QuantTree.EWMA_QuantTree(initial_pi_values, lambd, qt.tv_statistic, alpha, False, nu, desired_ARL0)
+    data_handler = aux.Data_set_Handler(data_Dimension)
+    data = data_handler.return_equal_batch(initial_data_set)
+    model.initialize(data)
+    stat = np.array(model.alterative_EWMA_thresholds_computation())
+    for index in range(1, experiments):
+        stat += np.array(model.alterative_EWMA_thresholds_computation())
+    stat = stat / index
+    plt.plot(stat)
+    plt.title('thresholds')
+    plt.show()
+
+
+test_EWMA(1, 10 * desired_ARL0, desired_ARL0, qt.tv_statistic)
+test_EWMA(1, 10 * desired_ARL0, desired_ARL0, qt.pearson_statistic)
 plt.show()
 
+pl = []
+for index in range(100):
+    print(index)
+    pl.append(test_ARLO_once(400, qt.tv_statistic))
+plt.boxplot(pl, showmeans=True)
+plt.title('ARl0: expected is' + str(desired_ARL0))
+print('Mean is: ' + str(np.mean(np.array(pl))))
+plt.show()
